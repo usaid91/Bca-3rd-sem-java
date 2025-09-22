1fbbb5d5d450131749556e43835a9cedfc7f5ff4
Usaid Ahmad <usaidahmadkhan2387@gmail.com>
2025-09-22 23:10:18 +0530

    Delete week3 directory

--- week3/w3_10.java
+++
@@ -1,9 +0,0 @@
-public class w3_10 {
-    public static void main(String[] args) {
-        int r = 6;
-        double c = 2*Math.PI*r;
-
-        System.out.println("Radius = "+r);
-        System.out.println("Circumference = "+c);
-    }
-}
